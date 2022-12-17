da796471627212aacfd125d2cec6e56df311c1f4
Fran1o <pankha994@gmail.com>
2022-12-17 20:28:12 -0300

    model nuevo

--- comenapp/forms.py
+++ comenapp/forms.py
@@ -3,5 +3,5 @@ from django import forms
 class ComentForm(forms.Form):
     remitente = forms.CharField(max_length=30)  
     contenido = forms.CharField(max_length=1000)
-    fecha = forms.IntegerField()
+    fecha = forms.DateField()
 
--- comenapp/models.py
+++ comenapp/models.py
@@ -3,8 +3,13 @@ from datetime import datetime
 
 # Create your models here.
 
-class Coment(models.Model):
+class Coments(models.Model):
     
     remitente = models.CharField(max_length=30)
     contenido = models.CharField(max_length=1000)
-    fecha = models.IntegerField()
+    fecha = models.DateField()
+
+
+
+
+
