71c05253a8f555aa9b0e0862e530372106b17a9d
Jnieto <juancarlosn09@gmail.com>
2022-12-18 00:08:07 -0300

    test model5

--- comenapp/models.py
+++ comenapp/models.py
@@ -2,12 +2,12 @@ from django.db import models
 from datetime import datetime
 
 # Create your models here.
+
 class Coments(models.Model):
-    
+    algo = models.CharField(max_length=30)
     remitente = models.CharField(max_length=30)
     contenido = models.CharField(max_length=1000)
     fecha = models.DateField()
-
 
 
 
